59a7b2fceeaa9a977b51f488319902f426d475ce
Ganna <gigikaliento@gmail.com>
2025-05-19 15:44:09 +0500

    I created news and categories

---
+++ js/categories.js
@@ -0,0 +1,34 @@
+const BASE_URL = "https://webfinalapi.mobydev.kz"
+
+async function fetchAndRenderCategories() {
+    try {
+        const response = await fetch(`https://webfinalapi.mobydev.kz/categories`);
+        if (!response.ok) throw new Error(`Ошибка HTTP: ${response.status}`);
+
+        const categoriesArray = await response.json();
+        document.querySelector('.categories').innerHTML = categoriesArray.map(categories => `
+            <h3 class="categories__title">${categories.name}</h3>
+            <div class="categories__actions">
+                <a
+                href="./edit.html?id=${categories.id}"
+                class="button button--blue button--small"
+            >
+                Редактировать
+            </a>
+            <button
+                type="button"
+                class="button button--red button--small"
+                onclick="deleteNews(${categories.id})"
+            >
+                Удалить
+            </button>
+            </div>
+            `).join('');
+
+    } catch (error) {
+        console.error('Ошибка при получении категорий', error);
+    }
+};
+document.addEventListener('DOMContentLoaded', fetchAndRenderCategories)
+
+
